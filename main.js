3a7e18d7ba27c837c3961b4b529ba08ebe341d74
noriega146 <129232092+noriega146@users.noreply.github.com>
2024-06-24 23:49:00 -0400

    COmmit

---
+++ main.js
@@ -0,0 +1,39 @@
+async function start(){
+    const response = await fetch("https://dog.ceo/api/breeds/list/all")
+    const data = await response.json()
+    createBreedList(data.message)
+}
+
+start ()
+
+function createBreedList (breedList){
+    document.getElementById("breed").innerHTML = `
+    <select onchange="loadByBreed(this.value)">
+            <option>Choose a dog Breed </option>
+            ${Object.keys(breedList).map(function (breed) {
+                return `<option>${breed}</option>`
+            }).join('')}
+            </select>
+        `
+ }
+          
+async function loadByBreed(breed){
+   if(breed != "Choose a dog Breed "){
+        const response = await fetch (`https://dog.ceo/api/breed/${breed}/images`)
+        const data = await response.json()
+        console.log(data)
+        createSlideShow(data.message)
+   }
+    }
+
+    
+
+
+function createSlideShow(images){
+    document.getElementById("slideshow").innerHTML = `
+    <div class="slide" style="background-image: url('${images[0]}')"></div>
+   
+    `
+
+
+}
